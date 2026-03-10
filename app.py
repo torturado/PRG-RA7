0919e94eb59394b98365066e06760a1a42cc76b1
amintahtahdev <188072548+amintahtahdev@users.noreply.github.com>
2026-03-10 09:00:46 +0100

    Home fet amb css heredat de login
    Home fet amb css heredat de login

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__) 
 
-@app.route('/') 
-def home():
+@app.route('/')
+def login():
     return render_template("login.html") 
 
-@app.route('/registre') 
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+@app.route('/registre')
 def registre():
     return render_template("registre.html") 
 if __name__ == '__main__':
